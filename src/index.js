f82f49f211a3922b60b7f455e12eb8506063c7ce
Riley MacDonald <riley@Rileys-MacBook-Air.local>
2018-03-09 11:40:39 -0500

    Fixed conflicts

--- src/index.js
+++ src/index.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 const Rideshare = () => {
   <BrowserRouter>
-    <MuiThemeProvider muiTheme={muiTheme}>
+    <MuiThemeProvider>
       {/* <Provider store={store}> */}
       <Switch>
-        <Layout>
           <Route exact path="/" render={() => 'hello'} />
-        </Layout>
       </Switch>
       {/* </Provider> */}
     </MuiThemeProvider>
