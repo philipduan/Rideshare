d0fb4c30cadd5cd6b78aa088911ea3a8a34ca7b6
Riley MacDonald <riley@Rileys-MacBook-Air.local>
2018-03-09 15:34:27 -0500

    Added login functionality

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { Provider } from 'react-redux';
 import store from './Redux/store';
 import App from './Components/App/App';
+import SignIn from './Containers/Sign-In/SignIn';
+
 //biiiiii
 const Rideshare = () => {
   return (
@@ -14,7 +16,7 @@ const Rideshare = () => {
       <MuiThemeProvider>
         <Provider store={store}>
           <Switch>
-            <Route exact path="/" component={App} />
+            <Route exact path="/" component={SignIn} />
           </Switch>
         </Provider>
       </MuiThemeProvider>
