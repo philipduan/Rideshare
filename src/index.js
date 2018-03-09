0bec5afb23250cbd8a72ee13d6a6f72af9ea25b4
Riley MacDonald <riley@Rileys-MacBook-Air.local>
2018-03-09 11:35:10 -0500

    Added route config

--- src/index.js
+++ src/index.js
@@ -3,6 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Rideshare = () => {
+  <BrowserRouter>
+    <MuiThemeProvider muiTheme={muiTheme}>
+      {/* <Provider store={store}> */}
+      <Switch>
+        <Layout>
+          <Route exact path="/" render={() => 'hello'} />
+        </Layout>
+      </Switch>
+      {/* </Provider> */}
+    </MuiThemeProvider>
+  </BrowserRouter>;
+};
+
+ReactDOM.render(<Rideshare />, document.getElementById('root'));
 registerServiceWorker();
